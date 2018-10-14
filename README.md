4d6d7433e928a4bb0dd95fb5ac75c37682cfe18e
Gerald McAlister <gemisis@users.noreply.github.com>
2018-10-14 00:33:19 -0700

    First Website Version
    [Problem]
    Nothing in the project!
    
    [Solution]
    Added the baseline project. Contains a simple website that can be
    browsed through.
    
    [Testing]
    Manual testing currently. Will investigate automated testing at a later
    time. Note that when testing on a local web browser, Firefox must be
    used! This is due to how the config.json file is loaded!

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # personal-website
+This is the repository for my personal website. It's designed to be pretty static, so that it can be easily deployed to an AWS S3 bucket. The exception to this is the contact page, which has two options: A static one that reveals your email address in the code, and a dynamic one that requires an AWS Lambda setup to utilize.
+
+## Setup
+### Static Setup
+This section describes how to setup the purely static version of the website. Note that when doing so, your email address will be visible in the website's code!
+#### AWS S3
+Setup for this is easy: Just update the config.json file with your information, and upload the project to S3! Then use their tutorials for setting up a domain for the website, and voila, you're all set to go!
+#### GitHub
+For hosting on GitHub, clone this project, update the config.json file, and then navigate to http://username.github.io/personal-website to view the page (Don't forget to replace "username" with your GitHub username!)!
+### Dynamic Setup
+This section describes how to setup the website so that certain parts of the website can be done dynamically (ie: On an external server). This is of benifit as it can help keep personal information (such as your email address) private since it won't appear anywhere on the website (even in its code)! This is great for several reasons:
+
+* Spam bots won't be able to search your page for any personal information.
+* You can add additional logic to any server side components without needing to update the code in this project.
+* You can also add additional processing for anything going through it (such as creating a report about how many emails you are receiving).
+
+#### AWS Lambda
+TBD
---
+++ js/core.js
@@ -0,0 +1,124 @@
+/**
+  core.js
+
+  This file contains helper functions for setting up the various web pages. Note the use of createElement instead of jQuery to create the elements. This is because we actually get better performance using this method instead. This should make the pages load much faster! See https://stackoverflow.com/questions/268490/jquery-document-createelement-equivalent for more information.
+
+  */
+
+function generateNavBar(activePage) {
+    // Want to add more pages? You can do so here!
+    var links = [
+        {
+            text: "Home",
+            url: "index.html"
+        },
+        {
+            text: "Projects",
+            url: "projects.html"
+        },
+        {
+            text: "Work",
+            url: "work.html"
+        },
+        {
+            text: "About",
+            url: "about.html"
+        }
+    ];
+    links.forEach(function(link) {
+        // Create a list item and set its class to nav-item, as well as active if it is the active page.
+        var listItem = document.createElement("li");
+        var classValue = "nav-item";
+        if (activePage === link.text) {
+            classValue += " active";
+        }
+        listItem.setAttribute("class", classValue);
+
+        // We need to create a link item for each list item separately.
+        var linkElement = document.createElement("a");
+        linkElement.setAttribute("class", "nav-link");
+        linkElement.setAttribute("href", link.url);
+        linkElement.setAttribute("style", "text-transform: capitalize;");
+        linkElement.innerHTML = link.text;
+        listItem.appendChild(linkElement);
+
+        // Append each link item to the links section of the navbar.
+        document.getElementById("navbar_links").appendChild(listItem);
+    });
+
+    // Set the URl for when the user clicks the user name in the top left-hand corner.
+    document.getElementById("navbar_brand").setAttribute("href", links[0].url);
+}
+
+function createMetaTag(name, content) {
+    var name_meta = document.createElement("meta");
+    name_meta.setAttribute("name", name);
+    name_meta.setAttribute("content", content);
+    return name_meta;
+}
+
+function setupSocialMediaLinks(socialMediaItems, socialMediaDivElement) {
+    socialMediaItems.forEach(function(socialMedia) {
+        // Create our CSS style tag for when the user hovers over a social media icon.
+        var style = document.createElement("style");
+        style.type = "text/css";
+        style.rel = "stylesheet";
+        if (style.stylesheet) {
+            style.stylesheet.cssText += "#" + socialMedia.id + ":hover {";
+            style.stylesheet.cssText += "color: " + socialMedia.color;
+            style.stylesheet.cssText += "}";
+        } else {
+            style.appendChild(document.createTextNode("#" + socialMedia.id + ":hover {" + "color: " + socialMedia.color + "}"));
+        }
+
+        // Create our link tags and icon tags, putting the icon tag into the link tag.
+        var link = document.createElement("a");
+        link.setAttribute("href", socialMedia.link);
+        link.setAttribute("target", "_blank");
+        var icon = document.createElement("i");
+        icon.setAttribute("class", "fab fa-3x social " + socialMedia.icon);
+        icon.setAttribute("id", socialMedia.id);
+        link.appendChild(icon);
+
+        // Append the style and link tags for each social media icon to the socialMediaDiv element that was passed in.
+        socialMediaDivElement.appendChild(style);
+        socialMediaDivElement.appendChild(link);
+    });
+}
+
+function setupConfig(activePage) {
+    $.getJSON('config.json', function(data) {
+        // Setup our social media links.
+        setupSocialMediaLinks(data.social_medias, document.getElementById("social_media_links"));
+
+        // Create our page title based on the user from our config and the active page.
+        var pageTitle = data.user + " - " + activePage;
+
+        // Get our <head> tag.
+        var head = document.getElementsByTagName("head")[0];
+
+        // Append on a bunch of meta tags.
+        head.appendChild(createMetaTag("author", data.user));
+        head.appendChild(createMetaTag("description", data.description));
+        head.appendChild(createMetaTag("keywords", data.keywords.join(",")));
+
+        // Create a title meta tag separately since it has some differnt parameters.
+        var title_meta = document.createElement("meta");
+        title_meta.setAttribute("name", "title");
+        title_meta.setAttribute("title", pageTitle);
+        head.appendChild(title_meta);
+
+        // Create our <title> tag.
+        var title_tag = document.createElement("title");
+        title_tag.innerHTML = pageTitle;
+        head.appendChild(title_tag);
+
+        // Finally set the name used in the top left-hand corner.
+        document.getElementById("navbar_brand").innerHTML = data.user
+    });
+}
+
+function setupPage(activePage) {
+    generateNavBar(activePage);
+    setupConfig(activePage);
+}
